4a084b4d3753f3db57e7405c0bb0bf4a6efe1a45
kalsoomlachi99-spec <kalsoomlachi99l@gmail.com>
2026-04-07 17:18:56 +0500

    add commit

--- code.cpp
+++ code.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 int main() {
     cout << "Hello VS Code!" << endl;
-    
+
     /*----------------------------------*/
 
+    /*checking if a number is even or odd*/
 
+int num = 5;
+if (num%2 == 0){
+    cout << num << " is an even number";
+} else { 
+        cout << num << "is an odd number";
+
+}
+
+    /*----------------------------------*/
 
 
 
