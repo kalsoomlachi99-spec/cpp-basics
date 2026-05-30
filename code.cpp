cbe08c42fd2ef19f2ffe4706a16680b8995c6d2f
rashid-789 <rashidiyaoo7@gmail.com>
2026-05-30 12:11:13 +0500

    code update

--- code.cpp
+++ code.cpp
@@ -15,12 +15,43 @@ int getMaxSum(int mat[][3], int rows, int cols){
 }
 
 int main(){
-    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int rows =3;
-    int cols = 3;
+    // int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    // int rows =3;
+    // int cols = 3;
 
-    cout << getMaxSum(matrix, rows, cols) << endl;
+/*is a number prime or not*/
 
+int n;
+
+cout << "enter a number: ";
+cin >> n;
+
+bool isPrime = true;
+
+for(int i=2 ; i <= (n-1) ; i++){
+    if (n%i==0){
+       isPrime = false;
+       break;
+    } 
+}
+
+if(isPrime == true){
+    cout<<"prime";
+} else {
+    cout <<"!prime";
+}
+
+
+int n;
+cout << "enter a number: ";
+cin >> n;
+for(int i=2 ; i <= (n-1) ; i++){
+    if (n%i==0){
+       cout<<"not prime";
+    } else{
+        cout << "prime";
+    }
+}
 
 
     return 0;
