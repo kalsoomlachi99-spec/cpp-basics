5461f856b93c209b64e4229dd4f26d2d48ab533b
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-02 21:52:38 +0500

    Number guessing game project version 4 (random number)

--- projects/number-guessing-game.cpp
+++ projects/number-guessing-game.cpp
@@ -1,39 +1,56 @@
 #include<iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;    
 int main (){
   
     /*Number guessing game project version 4 (random number)*/
 
-    int secret_Num = 7;
+    srand(time(0)); // Seed the random number generator with the current time 
+    int secret_Num =  rand() % 100 + 1; 
     int guess_Num;
     int attempts = 0;
+    bool won = false;
 
-    cout << "To play game guess a number: ";
+     cout << "Guess a number between 1 and 100: ";
     cin >> guess_Num;
-        
-    if(guess_Num != secret_Num){
-        for(int i=0; i<100; i++){
-            secret_Num = rand() % 100 + 1; // Generate a random number between 1 and 100
-        }
-    }
-
- 
+    attempts++;
+   
     while (guess_Num != secret_Num){
-        if (guess_Num > secret_Num)
-        {
+        
+        if (guess_Num > secret_Num) {
             cout << "Too high! ";
-        }
-        else
-        {
+        } else {
             cout << "Too low! ";
         }
-        cout << "Wrong guess, try again: ";
+
+        cout << "try again: ";
         cin >> guess_Num;
-    
+                
         attempts++;
+
+            if (attempts >= 10) {
+            cout << "Game over! You've used all 10 attempts. The secret number was " << secret_Num << ".\n";    
+            break;
+        }
+
+    }
+    
+    if (guess_Num == secret_Num) {
+        won = true;
     }
 
-    cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+    if (won == true) {
+    cout << " Congratulations!\n The secret number was " << secret_Num << ".\n";
+    
+    if (attempts == 1) {
+    cout << "You guessed the number in 1 attempt." << endl;
+} else {
+    cout << "You guessed the number in " << attempts << " attempts." << endl;
+}
+    } else {
+        cout << "Try for another time!.\n";
+    }
 
     return 0;
 
