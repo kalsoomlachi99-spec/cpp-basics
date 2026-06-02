ffbf369782b281da6ce5db81633a5c690887b0a1
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-02 16:39:07 +0500

    Number guessing game project version 4 (random number)

--- projects/number-guessing-game.cpp
+++ projects/number-guessing-game.cpp
@@ -2,13 +2,21 @@
 using namespace std;    
 int main (){
   
-    /*Number guessing game project version 3 (keep guessing until correct)*/
+    /*Number guessing game project version 4 (random number)*/
 
     int secret_Num = 7;
     int guess_Num;
+    int attempts = 0;
 
     cout << "To play game guess a number: ";
     cin >> guess_Num;
+        
+    if(guess_Num != secret_Num){
+        for(int i=0; i<100; i++){
+            secret_Num = rand() % 100 + 1; // Generate a random number between 1 and 100
+        }
+    }
+
  
     while (guess_Num != secret_Num){
         if (guess_Num > secret_Num)
@@ -21,9 +29,11 @@ int main (){
         }
         cout << "Wrong guess, try again: ";
         cin >> guess_Num;
+    
+        attempts++;
     }
 
-    cout << "Congratulations! You guessed the number." << endl;
+    cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
 
     return 0;
 
