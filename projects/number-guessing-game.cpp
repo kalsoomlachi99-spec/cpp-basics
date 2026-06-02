f681e700f1a018a9798daf2fd63bc3bf47c42052
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-02 16:12:25 +0500

    Number guessing game project version 2

--- projects/number-guessing-game.cpp
+++ projects/number-guessing-game.cpp
@@ -2,14 +2,22 @@
 using namespace std;    
 int main (){
   
-    /*Number guessing game project*/
+    /*Number guessing game project version 2*/
 
     int secret_Num = 7;
     int guess_Num;
 
     cout << "To play game guess a number: ";
     cin >> guess_Num;
-
-    (secret_Num == guess_Num) ? cout << "Congratulations! You guessed the number!" : cout << "Sorry! Try again.";
+ 
+    if (guess_Num == secret_Num){
+        cout << "Congratulations! You guessed the number.";
+    }
+    else if (guess_Num < secret_Num){
+        cout << "Too low! Try again.";
+    }
+    else {
+        cout << "Too high! Try again.";
+    }
 
 }
