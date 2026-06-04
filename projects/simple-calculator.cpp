4e2c37ca2addf203745210327acbe57a011daef7
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-04 15:52:52 +0500

    Bulding Up A Simple Calculator Project

---
+++ projects/simple-calculator.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    /*A Simple Calculator*/
+
+    int a , b ;
+    char op;
+
+    cout << "\nEnter first number: ";
+    cin >> a ;
+    cout << "\nEnter any one operator ( + , - , * , % , / ): " ;
+    cin >> op;
+    cout << "\nEnter second number: ";
+    cin >> b ;
+
+    switch (op) {
+    case  '+' :
+    cout << "Result = " << a + b ;
+    break;
+    case '-' :
+    cout << "Result = " << a - b ;
+    break;
+    case  '*' :
+    cout << "Result = " << a * b ;
+    break;
+    case '%' :
+    if ( b == 0){
+        cout << "Invalid calculation!" ;
+    } else {
+        cout << "Result = " << a % b ;
+    }
+    break;
+    case '/' :
+    if ( b == 0){
+        cout << "Can't divide by zero" ;
+    } else {
+    cout << "Result = " << a / b ;
+    }
+    break;
+    default :
+    cout << "Invalid calculation!" ;
+    break;
+    }
+    
+    cout << "\nThanks for using a simple calulator!";
+
+
+
+    return 0;
+}
