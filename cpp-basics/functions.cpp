64216c17f55a3671939689b0eb7ed4225ff58c4e
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-10 16:06:37 +0500

    sum of two numbers using function

--- cpp-basics/functions.cpp
+++ cpp-basics/functions.cpp
@@ -14,12 +14,17 @@ double min_of_two_numbers (double a, double b){
 
 }
 
+// sum of two numbers
+double sum_of_two_numbers (double a, double b){
+    return a + b;
+}
+
 int main(){
 
     // funtion call / invoke
  
  cout << min_of_two_numbers(10.5, 20.5) << endl;  // min of two numbers
-
+ cout << "sum = " << sum_of_two_numbers(10.5, 20.5) << endl; // sum of two numbers      
 
     return 0;
 }
