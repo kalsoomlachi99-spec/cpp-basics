679f44f821056781251ad863449abcf97ff30d33
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-11 15:21:53 +0500

    sum of n prime numbers

--- cpp-basics/functions.cpp
+++ cpp-basics/functions.cpp
@@ -115,19 +115,68 @@ string prime(int n){
     return "prime";
 }
 
+//sum of n prime numbers
+bool isPrime(int n){
+
+    if (n <= 1) {
+        return false;
+    }
+
+    for (int i = 2 ; i*i <= n ; i++){
+        if (n % i ==0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int sumOfPrimes(int num){
+    int sum = 0 ;
+    for (int i = 2 ; i <= num ; i++){
+        if (isPrime(i)){
+            sum += i;
+        }
+
+    } 
+    return sum;
+}
+
+
+
+
+
+// int sumOfNPrimes(int n){
+//     int sum = 0;
+//     int count = 0;
+//     int num = 2;
+
+//     while (count < n){
+//         if (isPrimes(num)){
+//             sum += num;
+//             count++;
+//         }
+//         num++;
+//     }
+//     return sum;
+// }
+
+
+
 
 int main(){
 
     // funtion call / invoke
  
- cout << "Minimum of two number is: " << min_of_two_numbers(4.5, 3.5) << endl;  // min of two numbers
- cout << "sum = " << sum_of_two_numbers(1.3, 2.5) << endl; // sum of two numbers      
- cout << "Sum on n numbers is: " << sumOfNum(49) << endl; // sum of numbers from 1 to n
- cout << "N factorial: " << nFact(4) << endl ;  // N factorial
- cout << sumOfDigit(1234) << endl;  // sum of digits of a number
- cout << nCR(4 , 2) << endl;  // nCr binomial coefficient for n and r (my version)
- cout << nCr(4 , 2) << endl;  // nCr binomial coefficient for n and r
- cout << prime(5) << endl; // prime number checker
+//  cout << "Minimum of two number is: " << min_of_two_numbers(4.5, 3.5) << endl;  // min of two numbers
+//  cout << "sum = " << sum_of_two_numbers(1.3, 2.5) << endl; // sum of two numbers      
+//  cout << "Sum on n numbers is: " << sumOfNum(49) << endl; // sum of numbers from 1 to n
+//  cout << "N factorial: " << nFact(4) << endl ;  // N factorial
+//  cout << sumOfDigit(1234) << endl;  // sum of digits of a number
+//  cout << nCR(4 , 2) << endl;  // nCr binomial coefficient for n and r (my version)
+//  cout << nCr(4 , 2) << endl;  // nCr binomial coefficient for n and r
+//  cout << prime(5) << endl; // prime number checker
+cout << sumOfPrimes(10) << endl; // sum of n prime numbers
 
  return 0;
 }
