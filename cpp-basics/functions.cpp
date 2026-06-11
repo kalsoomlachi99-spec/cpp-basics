aea4b673ddf9ed0696690b49f8d364772dd5f6c5
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-11 10:42:25 +0500

    practice problems using functions

--- cpp-basics/functions.cpp
+++ cpp-basics/functions.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 // function definition
 
-// min of two numbers
+// minimum two numbers
 double min_of_two_numbers (double a, double b){
 
     if ( a < b ) {
@@ -19,12 +20,114 @@ double sum_of_two_numbers (double a, double b){
     return a + b;
 }
 
+// sum of numbers from 1 to n
+int sumOfNum(int n){
+    int sum = 0;
+
+    for (int i = 1; i <= n ; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+// Calculate N factorial
+
+int nFact(int n){
+    int fact = 1;
+
+    for (int i = 1; i <= n ; i++){
+        fact *= i;
+    }
+
+    return fact;
+}
+
+// sum of digits of a number
+
+int sumOfDigit(int num){
+
+    int digitSum = 0;
+    
+    while (num > 0){
+       int lastDigit = num % 10;
+       num /= 10;
+       digitSum += lastDigit;
+    }
+    return digitSum;
+
+}
+
+// calculate nCr binomial coefficient for n and r (my version)
+
+int nCR(int n , int r){
+    int nFact = 1;
+    int rFact = 1;
+    int n_rFact = 1;
+
+    for (int i = 1; i <= n; i++) { // n factorial
+        nFact *= i;
+    }
+
+    for (int j = 1; j <= r; j++) {  // r factorial
+        rFact *= j;
+    }
+  
+    int n_r = n - r;
+
+    for (int k = 1; k <= r; k++) {  // n-r factorial
+        n_rFact *= k;
+    }
+   
+    return nFact / ( rFact * n_rFact );
+}
+
+// calculate nCr binomial coefficient for n and r 
+
+int factorial (int n){
+    int fact = 1;
+    for (int i = 1; i <= n ; i++){
+        fact *= i;
+    } 
+    return fact;
+}
+
+int nCr (int n , int r){
+    int nFact = factorial(n);
+    int rFact = factorial(r);
+    int n_rFact = factorial(n-r);
+    return nFact / ( rFact * n_rFact );
+
+}
+
+// calculate prime number using function
+
+string prime(int n){
+
+    if (n <= 1){
+        return "not prime";
+    } else {
+        for (int i = 2 ; i * i <= n ; i++){
+            if (n % i == 0){
+                return "not prime";
+            }
+        }
+    }
+    return "prime";
+}
+
+
 int main(){
 
     // funtion call / invoke
  
- cout << min_of_two_numbers(10.5, 20.5) << endl;  // min of two numbers
- cout << "sum = " << sum_of_two_numbers(10.5, 20.5) << endl; // sum of two numbers      
+ cout << "Minimum of two number is: " << min_of_two_numbers(4.5, 3.5) << endl;  // min of two numbers
+ cout << "sum = " << sum_of_two_numbers(1.3, 2.5) << endl; // sum of two numbers      
+ cout << "Sum on n numbers is: " << sumOfNum(49) << endl; // sum of numbers from 1 to n
+ cout << "N factorial: " << nFact(4) << endl ;  // N factorial
+ cout << sumOfDigit(1234) << endl;  // sum of digits of a number
+ cout << nCR(4 , 2) << endl;  // nCr binomial coefficient for n and r (my version)
+ cout << nCr(4 , 2) << endl;  // nCr binomial coefficient for n and r
+ cout << prime(5) << endl; // prime number checker
 
-    return 0;
+ return 0;
 }
