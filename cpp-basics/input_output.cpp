92034d398509e14ea440bf6ef6c308a9e2d98374
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-16 09:51:57 +0500

    print the number entered by user , only if , the number entered is negative

--- cpp-basics/input_output.cpp
+++ cpp-basics/input_output.cpp
@@ -3,18 +3,30 @@ using namespace std;
 int main(){
 
     // a number enter by user is  positive , negative or zero
-
     int x;
 
     cout << "Enter an integer: ";
     cin >> x ;
 
     if (x > 0){
-        cout << "The given number is positive." ;
+        cout << "\nThe given number is positive." ;
     } else if (x < 0) {
-        cout << "The given number is negative." ;
+        cout << "\nThe given number is negative." ;
     } else {
-        cout << "The given number is zero." ;
+        cout << "\nThe given number is zero." ;
     }
 
+    // print the number entered by user , only if , the number entered is negative
+    int y; 
+    
+    cout << "Enter an integer: ";
+    cin >> y ;    
+
+    if (y < 0){
+        cout << "\nThe number you enter is:" << y;
+    } else {
+        cout << "\nThe number you enter is not a negative number." ;
+    }
+
+  return 0;
 }
