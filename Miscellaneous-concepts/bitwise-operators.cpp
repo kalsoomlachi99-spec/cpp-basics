f6af8dca6ea139d26e21a7eda9e2a34717f5aebe
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-06-26 10:31:32 +0500

    data type modifier

--- Miscellaneous-concepts/bitwise-operators.cpp
+++ Miscellaneous-concepts/bitwise-operators.cpp
@@ -5,11 +5,15 @@ int main() {
 
     // Bitwise operators
 
-    cout << (6 & 10) << endl; //2  [BITWISE AND]
-    cout << (6 | 10) << endl; //14  [BITWISE OR]
-    cout << (6 ^ 10) << endl; //12  [BITWISE XOR]
-    cout << (10 << 2) << endl; // 40 (a * 2^b) [BITWISE LEFT SHIFT OPERATOR]
-    cout << (10 >> 1) << endl; //5 (a * 2^b)  [BITWISE RIGHT SHIFT OPERATOR]
+    cout << "[BITWISE AND]: (6 & 10) = " << (6 & 10) << endl; //2  [BITWISE AND]
+    
+    cout << "[BITWISE OR]: (6 | 10) = " << (6 | 10) << endl; //14  [BITWISE OR]
+    
+    cout << "[BITWISE XOR]: (6 ^ 10) = " << (6 ^ 10) << endl; //12  [BITWISE XOR]
+    
+    cout << "[BITWISE LEFT SHIFT OPERATOR]: (10 << 2) = " << (10 << 2) << endl; // 40 (a * 2^b) [BITWISE LEFT SHIFT OPERATOR]
+    
+    cout << "[BITWISE RIGHT SHIFT OPERATOR]: (10 >> 2) = " << (10 >> 1) << endl; //5 (a * 2^b)  [BITWISE RIGHT SHIFT OPERATOR]
 
     return 0;
 }
