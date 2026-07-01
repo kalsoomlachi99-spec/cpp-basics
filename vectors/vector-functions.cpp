e6d17cf71f468c22cfeb79bfbc8554097ff8a1e9
kalsoomlachi99 <kalsoomlachi99@gmail.com>
2026-07-01 23:57:44 +0500

    size and push_back functions in vector

---
+++ vectors/vector-functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // functions in vector
+
+    vector <int> vec;
+
+    cout << "Size before push back = " << vec.size() << endl;
+
+    vec.push_back(23);
+    vec.push_back(45);
+    vec.push_back(56);
+    
+    cout << "Size after push back = " << vec.size() << endl;
+
+    
+    return 0;
+}
