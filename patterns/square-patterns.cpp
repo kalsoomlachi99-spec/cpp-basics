4ac6d2653aa2d757aadee96aab4cdfc87e27377f
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-16 19:57:53 +0500

    Square pattern no 2 - 2 versions

--- patterns/square-patterns.cpp
+++ patterns/square-patterns.cpp
@@ -10,7 +10,7 @@ int main() {
     
     /*Square Patterns*/
 
-    int n = 4;
+    int n = 3;
 
     cout << "Square Pattern no 1:" << endl;
     //version 1:
@@ -37,6 +37,30 @@ int main() {
     for (int i = 0; i < n; i++){ 
         for(int j = 0; j < n; j++){
             cout << "* ";
+        }
+        cout << endl;
+    }
+    line();
+
+    cout << "Square Pattern no 2: " << endl;
+    // version 1:
+    int nums = 1;
+    for (int i = 0; i < n; i++){ 
+        for(int j = 0; j < n; j++){
+            cout << nums << " ";
+            nums++;
+        }
+        cout << endl;
+    }
+    line();
+
+    //version 2:
+
+    char ch = 'A';
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++) {
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
