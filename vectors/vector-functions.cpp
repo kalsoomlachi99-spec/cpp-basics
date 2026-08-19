123c279cbdc474cb0dca7e4eee2e7fe15c8ca2ca
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-19 20:38:16 +0500

    Insert function in vector

--- vectors/vector-functions.cpp
+++ vectors/vector-functions.cpp
@@ -14,18 +14,17 @@ int main() {
 
     cout << "\n===== Size Function =====" << endl;
 
-    cout << "Size = " << vec.size() << endl;
+    cout << "Initial size of vector is: " << vec.size() << endl;
 
     line();
 
-    cout << "===== Push Back Function =====" << endl;
+    cout << "===== Push Back Function =====" << endl; 
 
-    vec.push_back(1);
+    vec.push_back(1); // adding a value at the end of a vector
     vec.push_back(2);
     vec.push_back(3);
     vec.push_back(4);
     vec.push_back(5);
-
 
     cout << "values after push back function: ";
     for (int val : vec){
@@ -39,7 +38,7 @@ int main() {
 
     cout << "===== Emplace Back Function =====" << endl;
 
-    vec.emplace_back(6);
+    vec.emplace_back(6); // adding a value at the end of a vector
 
     cout << "values after emplace back function: ";
     for (int val : vec){
@@ -49,9 +48,11 @@ int main() {
     
     cout << "Size after emplace back function = " << vec.size() << endl;
 
-    cout << "===== Pop Back Function =====" << endl;
+    line();
 
-    vec.pop_back();
+    cout << "===== Pop Back Function =====" << endl; 
+
+    vec.pop_back(); // removing a value from the end of a vector
 
     cout << "values after pop back function: ";
     for (int val : vec){
@@ -89,9 +90,18 @@ int main() {
 
     cout << "===== Erase Function =====" << endl;
 
-    vec.erase(vec.begin() + 1); // erase the value at index 1
+    //iterators -> works like a pointer but not a pointer
+    vec.erase(vec.begin()); // erase the value at index 1
 
-    cout << "values after erase function: ";
+    cout << "values after erase function (specific index): ";
+    for (int val : vec){
+        cout << val << " ";
+    }
+    cout << endl;
+
+    vec.erase(vec.begin()+1, vec.begin()+2); // use to erase a range of values
+
+    cout << "values after erase function (range of values): ";
     for (int val : vec){
         cout << val << " ";
     }
@@ -100,6 +110,16 @@ int main() {
     cout << "Size after erase function = " << vec.size() << endl;
 
     line();
+
+    cout << "===== Insert Function =====" << endl;
+    
+    vec.insert(vec.begin()+3, 307); //(position, value) insert a value at a specific position
+
+    cout << "values after insert function: ";
+    for (int val : vec){
+        cout << val << " ";
+    }
+    cout << endl;
     
     return 0;
 }
