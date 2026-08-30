2ec122959c9344ad1369976496530dc856ddc660
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-30 15:47:17 +0500

    cstring- type

--- strings/character-array.cpp
+++ strings/character-array.cpp
@@ -7,10 +7,13 @@ int main() {
     
     /*CString or Character Array*/
 
-    char str[] = {'m', 'a', 'n', 'a', 'l', '\0'}; // '\0' is called a NULL character. size of \0 is 1byte, and is count as single character
+    char str1[] = "Hello!"; // string literals - literals: constant value
+    char str2[] = {'W', 'o', 'r', 'l', 'd', '\0'}; // '\0' is called a NULL character. size of \0 is 1byte, and is count as single character
 
-    cout << "str = " << str << endl;
-    cout << "size of str = " << strlen(str) << endl;
+    cout << "Str1[1] = " << str1[1] << endl;
+
+    cout << "Str2 = " << str2 << endl;
+    cout << "Size of str2 = " << strlen(str2) << endl;
 
     return 0;
 }
